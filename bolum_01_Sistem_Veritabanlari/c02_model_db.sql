038509d80c86857b60b4d30fd968af5e8593f242
omeryildizce <omeryildizce@gmail.com>
2022-10-28 15:29:47 +0300

    Delete c02_model_db.sql

--- bolum_01_Sistem_Veritabanlari/c02_model_db.sql
+++
@@ -1,6 +0,0 @@
----- Model DB
--- Þablon veri tabanýdýr.
--- Her bir oluþturulacak veritabaný ModelDb'nin bir kopyasý olarak oluþturulur.
--- Her veritabanýnda otomatik olarak olmasýný istediðimiz tipler, fonksiyonlar, tablolar vs
--- varsa bu veritabaný içine konulabilir.
-
