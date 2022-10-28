4423a1559b19921c78fea62512b056e982b3f89a
omeryildizce <omeryildizce@gmail.com>
2022-10-28 14:43:41 +0300

    Create c01_master_db.sql

---
+++ bolum_01_Sistem_Veritabanlari/c01_master_db.sql
@@ -0,0 +1,10 @@
+---- Master DB
+-- Sistem Konfigürasyonu
+-- Kullanýcýlar
+-- Veritabanlarý
+-- Sistem Dosyalarý
+-- Collation Bilgisi gibi Sql Server sisteminin temel konfigürasyon bilgsini tutar.
+
+-- Master db olmazsa sql server çalýþmaz
+
+
